119dab93943be067a534705d6c77ff7972198c74
William Niemiec <williamniemiec@hotmail.com>
2021-09-25 08:42:47 -0300

    TabNavigator - fix

--- src/navigators/HomeStack.js
+++ src/navigators/HomeStack.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
-import ConfigScreen from '../screens/ConfigScreen';
 import TaskScreen from '../screens/TaskScreen';
-import CreateTaskScreen from '../screens/CreateTaskScreen';
 import HistoryScreen from '../screens/HistoryScreen';
-import CreateCategoryScreen from '../screens/CreateCategoryScreen';
 
 const StackNavigator = createStackNavigator();
 
-function MainStack() {
+function HomeStack() {
   return (
     <StackNavigator.Navigator>
       <StackNavigator.Screen name="HomeScreen" component={HomeScreen} />
-      <StackNavigator.Screen name="ConfigScreen" component={ConfigScreen} />
       <StackNavigator.Screen name="TaskScreen" component={TaskScreen} />
-      <StackNavigator.Screen name="CreateTaskScreen" component={CreateTaskScreen} />
       <StackNavigator.Screen name="HistoryScreen" component={HistoryScreen} />
-      <StackNavigator.Screen name="CreateCategoryScreen" component={CreateCategoryScreen} />
     </StackNavigator.Navigator>
   );
 }
 
-export default MainStack;
+export default HomeStack;
