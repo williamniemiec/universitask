bc118ee5b1eac9693df934c05ebd429b4337613b
William Niemiec <williamniemiec@hotmail.com>
2021-10-01 15:01:32 -0300

    Keyboard hides tab bar

--- src/components/CustomTabBar/index.js
+++ src/components/CustomTabBar/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableHighlight, View } from 'react-native';
 import styles from './styles'
+import { Keyboard } from 'react-native';
 
 function CustomTabBar({ state, descriptors, navigation }) {
+
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setKeyboardVisible(true); // or some other action
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardVisible(false); // or some other action
+      }
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+
+  if (isKeyboardVisible)
+    return <></>
 
   return (
     <View style={styles.container}>
