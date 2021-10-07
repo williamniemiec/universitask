5f80addbffbd5ce1321fe45cabc46a24ea96740a
William Niemiec <williamniemiec@hotmail.com>
2021-10-07 06:29:11 -0300

    Refactoring

--- src/util/DateUtils.js
+++ src/util/DateUtils.js
@@ -1,15 +1,37 @@
 export default {
 
+  /**
+   * Formats a date in a friendly format.
+   * 
+   * @param   {Date} date Date to be formated
+   * @param   {bool} monthFirst [default=false] Indicates if the month should come
+   * before the day
+   * 
+   * @return  {string} Formated date
+   */
   formatDate: (date, monthFirst=false) => {
-    const min = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
-    const h = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
-    const m = (date.getMonth() + 1 < 10) ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
-    const d = (date.getDate() < 10) ? '0' + date.getDate() : date.getDate();
-    
-    const fixedTerms = "/" + date.getFullYear() + "  " + h + ":" + min;
+    const minutes = _getNumberWithLeadingZero(date.getMinutes());
+    const hours = _getNumberWithLeadingZero(date.getHours());
+    const month = _getNumberWithLeadingZero(date.getMonth());
+    const day = _getNumberWithLeadingZero(date.getDate());
 
     return  monthFirst 
-            ? (m) + "/" + (d) + fixedTerms
-            : (d) + "/" + (m) + fixedTerms;
+            ? _generateFormatedDateWithMonthFirst(day, month, year, hours, minutes)
+            : _generateFormatedDate(day, month, year, hours, minutes);
+  },
+
+  _getNumberWithLeadingZero: (number) => {
+    if (number >= 10)
+      return number;
+    
+    return '0' + number;
+  },
+
+  _generateFormatedDateWithMonthFirst: (day, month, year, hours, minutes) => {
+    return `${month}/${day}/${year} ${hours}:${minutes}`;
+  },
+
+  _generateFormatedDate: (day, month, year, hours, minutes) => {
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
   }
 }
