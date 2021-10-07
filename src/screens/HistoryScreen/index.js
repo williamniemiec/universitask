b29d6ad43b8ed0f644808d688e93a330efe7bc8d
William Niemiec <williamniemiec@hotmail.com>
2021-10-06 22:19:50 -0300

    Refactoring

--- src/screens/HistoryScreen/index.js
+++ src/screens/HistoryScreen/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Container from '../../components/template/Container';
-import { Area, Title } from './styled';
 
-function HistoryScreen() {
+const TaskScreen = ({ route }) => {
+  
   return (
     <Container>
-      <Area>
-        <Title>HistoryScreen</Title>
-      </Area>
+      
     </Container>
   );
 }
 
-export default HistoryScreen;
+export default TaskScreen;
